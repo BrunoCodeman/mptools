8ac4aaedfb58d3437158985fb357d8056f82c502
Bruno Codeman <brunobemfica@gmail.com>
2016-06-16 01:26:54 -0300

    new test folders

---
+++ tests/functional_tests/BaseTest.php
@@ -0,0 +1,14 @@
+<?php 
+require_once './MPTools/MPTools.php';
+
+/**
+* 
+*/
+class BaseTest implements IBaseTest extends PHPUnit_Framework_TestCase
+{
+    
+    function __construct(argument)
+    {
+        # code...
+    }
+}
